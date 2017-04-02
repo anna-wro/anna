dd0686bff259814c6ffea0ce5e88a4a7e84bb7f6
anna-wro <wro.online@gmail.com>
2017-04-02 12:53:17 +0200

    :lipstick: Hiding side nav

--- js/main.js
+++ js/main.js
@@ -83,14 +83,13 @@ $(function () {
 
 $(document).ready(function () {
     $(document).on("scroll", onScroll);
-
     //smooth scroll
-    $('.menu a').on('click', function (e) {
+    $('.dot_nav a').on('click', function (e) {
         e.preventDefault();
         $(document).off("scroll");
         console.log('click');
 
-        $('.menu a').each(function () {
+        $('.dot_nav a').each(function () {
             $(this).removeClass('active');
         })
         $(this).addClass('active');
@@ -109,16 +108,21 @@ $(document).ready(function () {
 
 function onScroll(event) {
     var scrollPos = $(document).scrollTop();
-    $('.menu a').each(function () {
+    $('.dot_nav a').each(function () {
         var currLink = $(this);
         var refElement = $(currLink.attr("href"));
         var top = refElement.position().top - 100;
         if (top <= scrollPos && top + refElement.height() > scrollPos) {
-            $('.menu a').removeClass("active");
+            $('.dot_nav a').removeClass("active");
             currLink.addClass("active");
         }
         else {
             currLink.removeClass("active");
         }
     });
+    if ($(".active")[0]){
+        $(".dot_nav").css('visibility', 'visible');
+    } else {
+        $(".dot_nav").css('visibility', 'hidden');
+    }
 }
