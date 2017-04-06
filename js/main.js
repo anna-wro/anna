066f8fdfba14daa7ae285075771c9d4821edd1cf
anna-wro <wro.online@gmail.com>
2017-04-06 10:54:57 +0200

    :cherries: Mobile styles

--- js/main.js
+++ js/main.js
@@ -1,29 +1,5 @@
 /*
- * Smooth scrolling
- */
-
-$(function () {
-    $('a[href*="#"]:not([href="#"])').click(function () {
-        if (location.pathname.replace(/^\//, '') === this.pathname.replace(/^\//, '') && location.hostname === this.hostname) {
-            var target = $(this.hash);
-            target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-            if (target.length) {
-                $('html, body').animate({
-                    scrollTop: target.offset().top
-                }, 1000);
-                return false;
-            }
-        }
-    });
-});
-
-
-$('figure').hover(function () {
-    $(this).next("div").children("hr").toggleClass("violet");
-});
-
-/*
- * Mail
+ * Taking care of the form
  */
 
 $(function () {
@@ -78,57 +54,19 @@ $(function () {
 
 });
 
+
 /*
- * Scroll spy
+ * Projects - effect on hover
  */
 
-$(document).ready(function () {
-    $(document).on("scroll", onScroll);
-    //smooth scroll
-    $('.dot_nav a').on('click', function (e) {
-        e.preventDefault();
-        $(document).off("scroll");
-        console.log('click');
-
-        $('.dot_nav a').each(function () {
-            $(this).removeClass('active');
-        });
-        $(this).addClass('active');
-
-        var target = this.hash,
-            menu = target;
-        $target = $(target);
-        $('html, body').stop().animate({
-            'scrollTop': $target.offset().top - 25
-        }, 500, 'swing', function () {
-            window.location.hash = target;
-            $(document).on("scroll", onScroll);
-        });
-    });
+$('figure').hover(function () {
+    $(this).next("div").children("hr").toggleClass("violet");
 });
 
-function onScroll() {
-    var scrollPos = $(document).scrollTop();
-    $('.dot_nav a').each(function () {
-        var currLink = $(this);
-        var refElement = $(currLink.attr("href"));
-        var top = refElement.position().top - 100;
-        if (top <= scrollPos && top + refElement.height() > scrollPos) {
-            $('.dot_nav a').removeClass("active");
-            currLink.addClass("active");
-        }
-        else {
-            currLink.removeClass("active");
-        }
-    });
-    if ($(".active")[0]){
-        $(".dot_nav").css('visibility', 'visible');
-    } else {
-        $(".dot_nav").css('visibility', 'hidden');
-    }
-}
+/*
+ * Close mobile nav on click
+ */
 
-// Close mobile nav onclick
 $(function () {
     $('.top_nav').find('a').on('click', closeNav);
 });
@@ -137,7 +75,10 @@ function closeNav() {
     jQuery("#burger").attr('checked', false);
 }
 
-// Change title when away
+/*
+ * Change title when away
+ */
+
 $(function() {
     var pageTitle = $("title").text();
     $(window).blur(function() {
