1664886a1d2822c12364c007d62c4e1dd8ab98f6
anna-wro <wro.online@gmail.com>
2017-04-15 13:55:02 +0200

    :package: Code style

--- js/main.js
+++ js/main.js
@@ -3,88 +3,84 @@
  */
 
 $(function () {
-
     // Get the form.
-    var form = $('#ajax-contact');
+  var form = $('#ajax-contact')
 
     // Get the messages div.
-    var formMessages = $('#form-messages');
+  var formMessages = $('#form-messages')
 
     // Set up an event listener for the contact form.
-    $(form).submit(function (e) {
+  $(form).submit(function (e) {
         // Stop the browser from submitting the form.
-        e.preventDefault();
+    e.preventDefault()
 
         // Serialize the form data.
-        var formData = $(form).serialize();
+    var formData = $(form).serialize()
 
         // Submit the form using AJAX.
-        $.ajax({
-                type: 'POST',
-                url: $(form).attr('action'),
-                data: formData
-            })
+    $.ajax({
+      type: 'POST',
+      url: $(form).attr('action'),
+      data: formData
+    })
             .done(function (response) {
                 // Make sure that the formMessages div has the 'success' class.
-                $(formMessages).removeClass('error');
-                $(formMessages).addClass('success');
+              $(formMessages).removeClass('error')
+              $(formMessages).addClass('success')
 
                 // Set the message text.
-                $(formMessages).text(response);
+              $(formMessages).text(response)
 
                 // Clear the form.
-                $('#name').val('');
-                $('#email').val('');
-                $('#message').val('');
+              $('#name').val('')
+              $('#email').val('')
+              $('#message').val('')
             })
             .fail(function (data) {
                 // Make sure that the formMessages div has the 'error' class.
-                $(formMessages).removeClass('success');
-                $(formMessages).addClass('error');
+              $(formMessages).removeClass('success')
+              $(formMessages).addClass('error')
 
                 // Set the message text.
-                if (data.responseText !== '') {
-                    $(formMessages).text(data.responseText);
-                } else {
-                    $(formMessages).text('Oops! An error occured and your message could not be sent.');
-                }
-            });
-
-    });
-
-});
-
+              if (data.responseText !== '') {
+                $(formMessages).text(data.responseText)
+              } else {
+                $(formMessages).text('Oops! An error occured and your message could not be sent.')
+              }
+            })
+  })
+})
 
 /*
  * Projects - effect on hover
  */
 
 $('figure').hover(function () {
-    $(this).next("div").children("hr").toggleClass("violet");
-});
+  $(this).next('div').children('hr').toggleClass('violet')
+})
 
 /*
  * Close mobile nav on click
  */
 
 $(function () {
-    $('.top_nav').find('a').on('click', closeNav);
-});
+  $('.top_nav').find('a').on('click', closeNav)
+})
 
-function closeNav() {
-    jQuery("#burger").attr('checked', false);
+function closeNav () {
+  jQuery('#burger').attr('checked', false)
 }
 
 /*
  * Change title when away
  */
 
-$(function() {
-    var pageTitle = $("title").text();
-    $(window).blur(function() {
-        $("title").text("Hey, come back! / " + pageTitle);
-    });
-    $(window).focus(function() {
-        $("title").text(pageTitle);
-    });
-});
+$(function () {
+  var pageTitle = $('title').text()
+  $(window).blur(function () {
+    $('title').text('Hey, come back! / ' + pageTitle)
+  })
+  $(window).focus(function () {
+    $('title').text(pageTitle)
+  })
+})
